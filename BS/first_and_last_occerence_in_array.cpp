67db8ec3d73963010ac2ac02a2c2d53a54c87cf2
Snehal Narale <snehalnarale05@gmail.com>
2026-08-03 15:53:10 +0530

    First and last occurence array

---
+++ BS/first_and_last_occerence_in_array.cpp
@@ -0,0 +1,58 @@
+int firstocc(vector<int> &arr, int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+int lastocc(vector<int> &arr, int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int key)
+{
+    pair<int, int> p;
+    p.first = firstocc(arr, n, key);
+    p.second = lastocc(arr, n, key);
+    return p;
+}
