9f3214695c7b0e88a050698b4b0b7c5aae4b2a85
Snehal Narale <snehalnarale05@gmail.com>
2026-08-08 00:08:55 +0530

    Bubble Sort

---
+++ Sorting.cpp/bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+void bubbleSort(vector<int> &arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+/* for(int i=0;i<n-1;i++){
+    for(int j=0;j<n-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+        }
+    }
+}
+*/
