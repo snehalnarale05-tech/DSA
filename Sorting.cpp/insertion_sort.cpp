7beec39976ba1539413aae17446ac662bb09ee6f
Snehal Narale <snehalnarale05@gmail.com>
2026-08-08 01:01:01 +0530

    Insertion Sort

---
+++ Sorting.cpp/insertion_sort.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+void insertionSort(int n, vector<int> &arr)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        for (; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int n, vector<int> &arr) {
+    for(int i = 1; i < n; i++) {
+
+        int temp = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
+*/
